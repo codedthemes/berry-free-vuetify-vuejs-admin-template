6ad3e67929284305e8c91aa07b829abc3ce080f2
ct-anjali-patel <anjali.rana@phoenixcoded.co>
2025-12-09 16:00:47 +0530

    all packages upgrades done, eslint prettier set & all pages checked done, eslint & build also checked (#32)

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginVue from 'eslint-plugin-vue';
+import prettierConfig from '@vue/eslint-config-prettier';
 
 export default [
   {
@@ -12,7 +13,33 @@ export default [
       }
     }
   },
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}']
+  },
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/node_modules/**']
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...pluginVue.configs['flat/essential']
+  ...pluginVue.configs['flat/essential'],
+  prettierConfig,
+  {
+    name: 'custom/prettier-rules',
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          bracketSpacing: true,
+          printWidth: 140,
+          singleQuote: true,
+          trailingComma: 'none',
+          tabWidth: 2,
+          useTabs: false,
+          endOfLine: 'lf'
+        }
+      ]
+    }
+  }
 ];
