710bec395f713c16b70dcc3e3d756a0ed8e55e4b
ct-anjali-patel <anjali.rana@phoenixcoded.co>
2026-04-14 16:35:36 +0530

    some changes done which is for seo changes, eslint error fix & build checked

--- src/layouts/full/footer/FooterPanel.vue
+++ src/layouts/full/footer/FooterPanel.vue
@@ -1,39 +1,20 @@
-<script setup lang="ts">
-import { shallowRef } from 'vue';
-
-const footerLink = shallowRef([
-  {
-    title: 'Home',
-    url: 'https://berrydashboard.com/vue/free'
-  },
-  {
-    title: 'Documentation',
-    url: 'https://codedthemes.gitbook.io/berry-vuetify/'
-  },
-  {
-    title: 'Support',
-    url: 'https://codedthemes.support-hub.io/'
-  }
-]);
-</script>
+<script setup lang="ts"></script>
 <template>
   <v-footer class="px-0 footer mt-2">
     <v-row justify="center" align="center" gap="0">
       <v-col cols="12" sm="6">
         <p class="text-body-medium mb-0 text-sm-left text-center">
           Berry ♥ crafted by Team
-          <a href="https://themeforest.net/user/codedthemes" class="text-primary footer-link" target="_blank">Codedthemes</a>
+          <a href="https://codedthemes.com/" class="text-primary footer-link" target="_blank">Codedthemes</a>
         </p>
       </v-col>
       <v-col class="text-sm-right text-center" cols="12" sm="6">
         <a
-          v-for="(item, i) in footerLink"
-          :key="i"
           class="mx-2 text-body-medium text-darkText text-decoration-none"
           target="_blank"
-          :href="item.url"
+          href="https://www.figma.com/community/file/1468460364009262125/berry-free-dashboard-ui-kit"
         >
-          {{ item.title }}
+          Free Figma UI kit
         </a>
       </v-col>
     </v-row>
